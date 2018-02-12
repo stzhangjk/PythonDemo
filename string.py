cb62cebfe3154e391c41b145256927018010c5d3
jikaizhang <jikaizhang@dj.cn>
2018-02-12 10:09:04 +0800

    字符串

---
+++ string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('123456')
+# 字符串长度
+print(len('asdasd'))
+# 字符串格式化
+print('hello %s' % 'world')
+# %% 百分号转义
+print('%d %s %f %x %%' % (16, 'string', 0.25233232323, 0xFF))
