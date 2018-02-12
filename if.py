3a519b2da231f70e7f1a328435c6ef3dca22d110
jikaizhang <jikaizhang@dj.cn>
2018-02-12 11:08:34 +0800

    条件判断

---
+++ if.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+age = 3
+if age >= 18:
+    print('adult')
+    print(1)
+elif age >= 6:
+    print('teenager')
+    print(2)
+else:
+    print('kid')
+    print(3)
