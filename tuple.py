a60f8db9f077f1879436341313aff5763a3d8d0b
jikaizhang <jikaizhang@dj.cn>
2018-02-12 11:03:58 +0800

    元组

--- tuple.py
+++ tuple.py
@@ -4,7 +4,17 @@
 # 元组
 tp = ('username','password',12)
 print(tp)
-
+# 元组元素1
 tp = (1,)
 print(tp)
+# 数字1
 print((1))
+
+tp = ('username', 'password')
+print(tp[1])
+
+tp = ('admin', '123456', [1, 2])
+print(tp)
+tp[2][0]=3
+tp[2][1]=4
+print(tp)
