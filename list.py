41d4ee09a1369ec02ce45b2d9dadcc314c9aac3d
jikaizhang <jikaizhang@dj.cn>
2018-02-12 10:24:26 +0800

    有序列表

---
+++ list.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+fruit = ['apple', 'banana', 'orange']
+
+print(fruit)
+
+print(len(fruit))
+
+print(fruit[0])
+print(fruit[2])
+
+# 最后一个元素
+print(fruit[-1])
+# 倒数第二个
+print(fruit[-2])
+
+# 追加元素
+fruit.append('asdhaskjd')
+print(fruit[-1])
+
+# 把元素插入到指定的位置，比如索引号为1的位置
+fruit.insert(1, 'insert')
+print(fruit)
+
+# 弹出末尾元素
+print(fruit.pop())
+print(fruit)
+
+# 弹出指定位置
+print(fruit.pop(1))
+print(fruit)
+
+fruit[1]='apple'
+print(fruit)
+
+arr = ['abc',12,True]
+print(arr)
+
+arrs = [
+    456,
+    ['abc', 213, True],
+    'abc'
+]
+print(arrs)
