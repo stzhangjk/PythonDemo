5fb8d7dbaca2cca841f456676a53f522358a3865
jikaizhang <jikaizhang@dj.cn>
2018-02-13 15:46:52 +0800

    切片

---
+++ slice.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 切片 左闭右开
+a = [1, 2, 3, 4, 5]
+print(a[:3])# 0省略
+print(a[1:3])
+print(a[:])
+print(a[::2])# 每两个取一个
+print(a[1::2])
+print("abcdefg"[:4])
+print((1, 2, 3, 4, 5, 6)[:5])
